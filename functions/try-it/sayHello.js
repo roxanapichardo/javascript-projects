80bf0cb07fee6cb9ec10b14a39829cad1a7096a9
Roxana Pichardo <roxypichardo@gmail.com>
2024-06-06 21:06:31 -0500

    Studio

--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -1,3 +1,34 @@
 function sayHello() {
    console.log("Hello, World!");
 }
+//nothing
+
+function plusTwo(num) {
+   return num + 2;
+}
+
+let a = 2;
+
+for (let i=0; i < 4; i++) {
+   a = plusTwo(a);
+}
+
+console.log(a);
+//returns 10
+
+function repeater(str) {
+   let repeated = str + str;
+   console.log(repeated);
+}
+
+repeater('Bob');
+
+//BobBob
+
+let num = 42;
+
+function isEven (num) {
+   return num % 2 === 0;
+}
+
+console.log(isEven(43));
